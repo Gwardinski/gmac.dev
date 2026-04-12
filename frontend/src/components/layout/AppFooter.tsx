68d446e0c5cea87f3e2246a1a9875c8ccb12f70b
Gordon <gordon@MacBookPro14.mynet>
2026-04-12 20:51:06 +0100

    fixup

--- frontend/src/components/layout/AppFooter.tsx
+++ frontend/src/components/layout/AppFooter.tsx
@@ -11,7 +11,7 @@ export const AppFooter: React.FC = () => {
           <TextAnchor href="https://github.com/Gwardinski">@Gwardinski</TextAnchor>
           <img src={cyndaquilGif} alt="" className="absolute -top-2 left-28 h-8 w-8" />
         </span>
-        <p className="text-gray-700 dark:text-gray-300">gordon.macintyre@proton.me</p>
+        <TextAnchor href="mailto:gordon.macintyre@proton.me">gordon.macintyre@proton.me</TextAnchor>
       </CardHeader>
     </Card>
   );
