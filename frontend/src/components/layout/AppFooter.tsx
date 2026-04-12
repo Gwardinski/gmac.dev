8f5fdf0e37c22290ebb929c931c6157300a91387
Gordon <gordon@MacBookPro14.mynet>
2026-04-12 20:48:26 +0100

    fixup

--- frontend/src/components/layout/AppFooter.tsx
+++ frontend/src/components/layout/AppFooter.tsx
@@ -1,3 +1,4 @@
+import cyndaquilGif from '../../assets/cyndaquil.gif';
 import { Card, CardHeader, TextAnchor } from '../gmac.ui';
 import { useVariantState } from '../VariantToggle';
 
@@ -8,7 +9,7 @@ export const AppFooter: React.FC = () => {
       <CardHeader column>
         <span className="relative flex items-center gap-2">
           <TextAnchor href="https://github.com/Gwardinski">@Gwardinski</TextAnchor>
-          <img src="/src/assets/cyndaquil.gif" className="absolute -top-2 left-28 h-8 w-8" />
+          <img src={cyndaquilGif} alt="" className="absolute -top-2 left-28 h-8 w-8" />
         </span>
         <p className="text-gray-700 dark:text-gray-300">gordon.macintyre@proton.me</p>
       </CardHeader>
